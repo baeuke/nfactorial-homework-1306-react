b43586202f2e8a498f41877b7db7a3c1c4a0df81
Baurzhan <baurzhan@Baurzhans-MacBook-Pro.local>
2022-06-14 10:50:13 +0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import {format} from 'date-fns';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='header'>
+        <div className='head1'>
+          <img className="nFimg" src="https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red.png" alt="<nFactorial logo>" />
+          <input type="text" />
+        </div>
+
+        <div className='date'>
+          {format(new Date(), 'dd.MM.yyyy hh:mm')}
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className="hey">
+          hey
+        </div>
+          
+        <div className="lets">
+          let's
+        </div>
+
+        <div className="give">
+          give
+        </div>
+
+        <div className="all">
+          all
+        </div>
+
+        <div className="youcan">
+          you can
+        </div>
+      </div>
+
+    </>
   );
 }
 
